28b1710527618da170f9b19220f42cfdbcb53f88
Adam Joulak <adam.joulak@se.experis.com>
2021-09-02 15:01:05 +0200

    Added controller, repository & model for User

--- src/main/java/com/example/alumniserver/controller/UserController.java
+++ src/main/java/com/example/alumniserver/controller/UserController.java
@@ -1,21 +1,37 @@
 package com.example.alumniserver.controller;
 
 
-import com.example.alumniserver.httpstatus.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.alumniserver.data_access.UserRepository;
+import com.example.alumniserver.models.User;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/api/v1/user")
+import java.util.ArrayList;
+
+@RestController
 public class UserController {
 
-    private final HttpStatusCode statusCode = new HttpStatusCode();
+    private UserRepository userRepository = new UserRepository();
 
-    @GetMapping
-    public ResponseEntity<String> getPosts() {
-        return new ResponseEntity<>("Test", statusCode.getFoundStatus("Test"));
+
+    @RequestMapping(value="/api/users", method = RequestMethod.GET)
+    public ArrayList<User> getAllUsers(){
+        return userRepository.getAllUsers();
     }
+
+    @RequestMapping(value = "api/user", method = RequestMethod.GET)
+    public User getUserByQueryId(@RequestParam(value="id", defaultValue = "ALFKI") int id){
+        return userRepository.getUserById(id);
+    }
+
+    @RequestMapping(value = "api/users/add", method = RequestMethod.POST)
+    public Boolean addNewUser(@RequestBody User user){
+        return userRepository.addUser(user);
+    }
+
+    @RequestMapping(value = "api/users/{id}", method = RequestMethod.PUT)
+    public Boolean UpdateUser(@PathVariable int id, @RequestBody User user){
+        return userRepository.updateUser(user);
+    }
+
 
 }
