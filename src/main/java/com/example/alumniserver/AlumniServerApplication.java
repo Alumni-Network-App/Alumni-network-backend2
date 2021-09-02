ec4a61c36453355c3d4a57ed63eea7dfe5da7815
Adam Joulak <adam.joulak@se.experis.com>
2021-09-02 11:57:38 +0200

    ny repo

---
+++ src/main/java/com/example/alumniserver/AlumniServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.alumniserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlumniServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alumniserver/AlumniServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alumniserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
