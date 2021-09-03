d556b5a44356212978ff2476af8568c17a5a4b30
Adam Joulak <adam.joulak@se.experis.com>
2021-09-03 13:51:05 +0200

    solving merge issues and added UserController

---
+++ src/main/java/com/example/alumniserver/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.example.alumniserver.controller;
+
+
+import com.example.alumniserver.data_access.UserRepository;
+import com.example.alumniserver.httpstatus.HttpStatusCode;
+import com.example.alumniserver.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private HttpStatusCode httpStatusCode = new HttpStatusCode();
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userRepository.findAll();
+        return new ResponseEntity<>(users, httpStatusCode.getFoundStatus(users));
+    }
+
+
+    //TODO
+    //Fixa LÄNKEN
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.SEE_OTHER)
+    public ResponseEntity<String> getUserByQueryId(@RequestHeader int user_id){
+        return new ResponseEntity<>("/user", HttpStatus.SEE_OTHER);
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ResponseEntity<User> addNewUser(@RequestBody User user){
+        return new ResponseEntity<>(userRepository.save(user), httpStatusCode.getContentStatus());
+    }
+
+    @Modifying
+    @RequestMapping(value = "/update", method = RequestMethod.PATCH)
+    public ResponseEntity<User> UpdateUser(@RequestBody int id, User user){
+        return new ResponseEntity<>(userRepository.save(user), httpStatusCode.getContentStatus());
+    }
+
+
+}
--- src/main/java/com/example/alumniserver/data_access/UserRepository.java
+++ src/main/java/com/example/alumniserver/data_access/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.alumniserver.data_access;
 
-import com.example.alumniserver.models.User;
+import com.example.alumniserver.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
