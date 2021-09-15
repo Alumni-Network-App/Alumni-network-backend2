52edee98b91eedc4648f034e2127ab6b6bc0fc74
Adam Joulak <adam.joulak@se.experis.com>
2021-09-15 15:30:09 +0200

    test

--- src/main/java/com/example/alumniserver/model/Rsvp.java
+++ src/main/java/com/example/alumniserver/model/Rsvp.java
@@ -23,32 +23,6 @@ public class Rsvp {
     @Column(name = "last_updated")
     private Date last_updated;
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    @Id
-    public String getId() {
-        return id;
-    }
-
-    public int getGuest_count() {
-        return guest_count;
-    }
-
-    public void setGuest_count(int guest_count) {
-        this.guest_count = guest_count;
-    }
-
-
-    public Date getLast_updated() {
-        return last_updated;
-    }
-
-    public void setLast_updated(Date last_updated) {
-        this.last_updated = last_updated;
-    }
-
     @Id
     @ManyToOne
     @JoinColumn(name = "userId", referencedColumnName = "userId")
